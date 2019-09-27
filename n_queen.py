bfd13e230388836a3309aee5b16c340a2cfb0a68
Jatin Modi <jatinmodi0001@gmail.com>
2019-09-28 00:21:10 +0530

    Adding comments and minor changes

--- n_queen.py
+++ n_queen.py
@@ -1,11 +1,11 @@
-def valid_move(chess,col,row):
+def valid_move(chess,col,row):				# finding the place for new queen if available
 	c=1
-	for i in range(col-1,-1,-1):
+	for i in range(col-1,-1,-1):	
 		if chess[i]==row:
 			return False
-		if chess[i]==row-c:
+		if chess[i]==row-c:			# to check that the position is safe from 1st diagonal
 			return False
-		if chess[i]==row+c:
+		if chess[i]==row+c:			# to check that the position is safe from 2nd diagonal
 			return False
 		c+=1
 	return True
@@ -15,8 +15,8 @@ def solve(chess,col,size):
 		return True
 	for i in range(size):
 		if valid_move(chess,col,i):
-			chess[col]=i
-			if solve(chess,col+1,size):
+			chess[col]=i 				# if position found than storing it in an array
+			if solve(chess,col+1,size):		# Recursize call for second row
 				return True
 	chess[col]=None
 	return False
@@ -24,22 +24,21 @@ def solve(chess,col,size):
 def solve_nqueen(size):
 	chess=[None]*size
 	if solve(chess,0,size):
-		print("solved")
+		print("Solved")
 		print(chess)
-		for i in chess:
-			arr=[0]*size
-			for j in range(0,size):
-				if i==j:
-					arr[size-1-j]='Q'
+		for val in chess:
+			arr=[0]*size					# declaring new array
+			for j in range(0,size):			
+				if val==j:	
+					arr[j]='Q'			# save 'Q' for the position of queen in that row
 				else:
-					arr[size-1-j]='.'
-			print(arr)
+					arr[j]='.'
+			print(arr)					# print array
 		return True
 	else:
-		print("No solution")
+		print("No Solution")
 		return False
 	
 if __name__=='__main__':
-	size=int(input("enter the size: "))
+	size=int(input("Enter the size: "))
 	solve_nqueen(size)
-
